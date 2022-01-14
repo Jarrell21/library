9a7d45299b60c96e5f9401cdf16484f8794b43db
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-01-14 23:42:17 +0800

    Add 'addBookBtn' functionality

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+const addBookBtn = document.querySelector('#add-book-btn');
+const listOfBooks = document.querySelector('.list-of-books');
+
+
+
+
+
+// Event listener for add book button
+addBookBtn.addEventListener('click', addBook);
+
+function addBook(){
+    const titleInput = prompt('Title: ');
+    const authorInput = prompt('Author: ');
+    const pagesInput = prompt('Pages: ');
+    const isReadInput = prompt('Have you read the book? ');
+
+    const newBook = document.createElement('div');
+    newBook.classList.add('book-card');
+
+    const newBookTitle = document.createElement('p');
+    newBook.classList.add('book-title');
+    newBookTitle.textContent = `Title: ${titleInput}`;
+
+    const newBookAuthor = document.createElement('p');
+    newBook.classList.add('book-author');
+    newBookAuthor.textContent = `Author: ${authorInput}`;
+
+    const newBookPages = document.createElement('p');
+    newBook.classList.add('book-pages');
+    newBookPages.textContent = `Pages: ${pagesInput}`;
+
+    const newBookIsRead = document.createElement('p');
+    newBook.classList.add('book-isRead');
+    newBookIsRead.textContent = `Already read: ${isReadInput}`;
+
+    const removeBookBtn = document.createElement('button');
+    removeBookBtn.classList.add('remove-book-btn')
+    removeBookBtn.textContent = 'Remove';
+
+
+    newBook.appendChild(newBookTitle);
+    newBook.appendChild(newBookAuthor);
+    newBook.appendChild(newBookPages);
+    newBook.appendChild(newBookIsRead);
+    newBook.appendChild(removeBookBtn);
+    listOfBooks.appendChild(newBook);
+}
+
+// Storage of all the books added
+let myLibrary = [];
+
+// Constructor for the books
 function Book(title, author, pages, isRead){
     this.title = title
     this.author = author
