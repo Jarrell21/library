5a74ef912efcc7a10db0699f585baea71ffda7fa
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-01-15 23:33:06 +0800

    Change displaying of books algorithm

--- script.js
+++ script.js
@@ -1,50 +1,8 @@
 const addBookBtn = document.querySelector('#add-book-btn');
 const listOfBooks = document.querySelector('.list-of-books');
 
-
-
-
-
 // Event listener for add book button
 addBookBtn.addEventListener('click', addBook);
-
-function addBook(){
-    const titleInput = prompt('Title: ');
-    const authorInput = prompt('Author: ');
-    const pagesInput = prompt('Pages: ');
-    const isReadInput = prompt('Have you read the book? ');
-
-    const newBook = document.createElement('div');
-    newBook.classList.add('book-card');
-
-    const newBookTitle = document.createElement('p');
-    newBook.classList.add('book-title');
-    newBookTitle.textContent = `Title: ${titleInput}`;
-
-    const newBookAuthor = document.createElement('p');
-    newBook.classList.add('book-author');
-    newBookAuthor.textContent = `Author: ${authorInput}`;
-
-    const newBookPages = document.createElement('p');
-    newBook.classList.add('book-pages');
-    newBookPages.textContent = `Pages: ${pagesInput}`;
-
-    const newBookIsRead = document.createElement('p');
-    newBook.classList.add('book-isRead');
-    newBookIsRead.textContent = `Already read: ${isReadInput}`;
-
-    const removeBookBtn = document.createElement('button');
-    removeBookBtn.classList.add('remove-book-btn')
-    removeBookBtn.textContent = 'Remove';
-
-
-    newBook.appendChild(newBookTitle);
-    newBook.appendChild(newBookAuthor);
-    newBook.appendChild(newBookPages);
-    newBook.appendChild(newBookIsRead);
-    newBook.appendChild(removeBookBtn);
-    listOfBooks.appendChild(newBook);
-}
 
 // Storage of all the books added
 let myLibrary = [];
@@ -54,17 +12,47 @@ function Book(title, author, pages, isRead){
     this.title = title
     this.author = author
     this.pages = pages
-    this.isRead = function(){
-        if (isRead === true) return 'already read';
-        return 'not read yet';
-    }
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${this.isRead()}`;
-    }
+    this.isRead = isRead
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-const spiderMan = new Book('The Spiderman', 'Steven Seagull', 300, true);
+function addBook(){
+    const bookTitle = prompt('Title: ');
+    const bookAuthor = prompt('Author: ');
+    const bookPages = prompt('Number of pages: ');
+    const bookHaveRead = prompt('Have you read the book?');
 
-console.log(theHobbit.info());
-console.log(spiderMan.info());
+    const book = new Book(bookTitle, bookAuthor, bookPages, bookHaveRead);
+    myLibrary.push(book);
+
+    displayBooks();
+}
+
+function displayBooks(){
+    listOfBooks.textContent = '';
+    myLibrary.forEach(book =>{
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const bookCardTitle = document.createElement('p');
+        bookCardTitle.textContent = `Title: ${book.title}`;
+
+        const bookCardAuthor = document.createElement('p');
+        bookCardAuthor.textContent = `Author: ${book.author}`;
+
+        const bookCardPages = document.createElement('p');
+        bookCardPages.textContent = `Pages: ${book.pages}`;
+
+        const bookCardIsRead = document.createElement('button');
+        bookCardIsRead.textContent = book.isRead;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+
+        bookCard.appendChild(bookCardTitle);
+        bookCard.appendChild(bookCardAuthor);
+        bookCard.appendChild(bookCardPages);
+        bookCard.appendChild(bookCardIsRead);
+        bookCard.appendChild(removeBtn);
+        listOfBooks.appendChild(bookCard);
+    })
+}
